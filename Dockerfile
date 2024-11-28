9ec4e3352486ea47a67b6b7eb1b0381d011e5926
Ines Prieto <66635557+ipl1988@users.noreply.github.com.>
2024-11-28 17:06:34 +0100

    docker & api

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
 FROM python:3.10.6-buster
 
-COPY requirements.txt
+COPY api api
+COPY requirements.txt requirements.txt
+COPY model.pkl model.pkl
+
+RUN pip install -r requirements.txt
+
+CMD uvicorn api.main:app --host 0.0.0.0
--- api/main.py
+++ api/main.py
@@ -23,88 +23,21 @@ app = FastAPI()
 def root():
     return {"message": "¡Hola, FastAPI!"}
 
+#model prediction to be returned in the API'''
+import pickle
+model_path = '/Users/ines/code/ipl1988/Hemorrhage_detection/model.pkl'
+file = open(model_path, "rb")
+app.state.model = pickle.load(file)
 
-@app.post("/testuploadfile/")
-async def test_upload_file (file: UploadFile = File(...)):
-    img = await file.read()
-
-    async with aiofiles.open("destination.png" , "wb") as f:
-        await f.write(img)
-
-    return {'description': 'just to test if image upload works',
-            'harcoded prediction' : [0.23,0.34,0.56,0.54,0.67,0.45]}
-
-# load model to system memory
-app.state.model = load_model("/Users/ines/code/ipl1988/Hemorrhage_detection/base_cat_dog")
-
+#endpoint model prediction
 @app.post("/prediction/")
 async def predictimage(file: UploadFile = File(...)):
     img_bytes = await file.read()
     img = Image.open(BytesIO(img_bytes))
     img = img.resize((150,150))
     img = img_to_array(img)
-    img = img.reshape((-1, 150, 150, 3))
-    res = app.state.model.predict(img)[0][0]
-    if(res < 0.5):
-        injury = "present"
-        prob = 1-res
-    if(res >= 0.5):
-        injury = "not present"
-        prob = res
+    img = img.reshape((-1, 150, 150, 1))
+    res = app.state.model.predict(img)
+    print(res)
 
-    print("injury: ", injury)
-    print("probability = ",prob)
-
-    return {"injury" : injury,
-            "probability" : prob}
-# model_new = load_model("/Users/ines/code/ipl1988/Hemorrhage_detection/base_cat_dog")
-
-
-# #make any image we upload compatible with the architecture of the dummy model
-# img=Image.open('/Users/ines/Desktop/sherry-christian-8Myh76_3M2U-unsplash.jpg')
-# img=img.resize((150,150))
-# img = img_to_array(img)
-# img = img.reshape((-1, 150, 150, 3))
-# res = model_new.predict(img)[0][0]
-# print(res)
-# THIS CODE MIGHT BE USEFUL ONLY IN CASE WE NEED TO LOAD THE IMAGE FROM AN API (I DONT THINK SO)
-# import requests
-# from io import BytesIO||/
-#def getImage(url):
-    #response = requests.get(url)
-    #img = Image.open(BytesIO(response.content))
-    #plt.imshow(img)
-    #img = img.resize((150, 150))
-    #return img
-
-
-
-#model prediction to be returned in the API'''
-
-########## TEST LOCAL ###########
-path='/Users/ines/Desktop/sherry-christian-8Myh76_3M2U-unsplash.jpg'
-model_new = load_model("/Users/ines/code/ipl1988/Hemorrhage_detection/base_cat_dog")
-
-def predictimage(img_path, model):
-
-    img = Image.open(img_path)
-    img=img.resize((150,150))
-    img = img_to_array(img)
-    img = img.reshape((-1, 150, 150, 3))
-    res = model.predict(img)[0][0]
-    if(res < 0.5):
-        injury = "present"
-        prob = 1-res
-    if(res >= 0.5):
-        injury = "not present"
-        prob = res
-
-    print("injury: ", injury)
-    print("probability = ",prob)
-
-
-    return injury, prob
-
-if __name__ == '__main__':
-    injury, prob = predictimage(path, model_new)
-    print(injury, ' ', prob)
+    return {"injury" : float(res)}
