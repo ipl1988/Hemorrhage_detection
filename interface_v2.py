79ed53a303061e3a96b8c92fe6785b33157f0e8e
Julia Ojeda Alonso <167695733+JuliaOjedaAlonso@users.noreply.github.com>
2024-12-05 11:37:38 +0100

    interface new v2 version

--- interface_v2.py
+++ interface_v2.py
@@ -173,13 +173,13 @@ def main():
 
                 if result>0.5:
                     # Display the prediction result
-                    st.success('<p style="font-size:20px; color:green;">Positive: high probability to find a hemorrhage in this image.</p>', unsafe_allow_html=True)
+                    st.success('<p style="font-size:20px; color:green;">Positive: high probability to find a hemorrhage in this image.</p>')
                 elif result<0.5:
-                    st.success('<p style="font-size:20px; color:green;">Negative: high probability to find a hemorrhage in this image.</p>', unsafe_allow_html=True)
+                    st.success('<p style="font-size:20px; color:green;">Negative: high probability to find a hemorrhage in this image.</p>')
                 else:
-                    st.error('<p style="font-size:20px; color:red;">Failed to get a prediction from the API.</p>', unsafe_allow_html=True)
+                    st.error('<p style="font-size:20px; color:red;">Failed to get a prediction from the API.</p>')
 
-    st.markdown("---")
+    st.markdown("---",unsafe_allow_html=True)
     st.markdown("### About")
     st.text("This tool uses a CNN deep learning model to predict injury types from uploaded images.")
 
