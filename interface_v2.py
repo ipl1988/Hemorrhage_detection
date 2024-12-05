f7248000a559a6311a39eb3d0acc6907fd80e5d4
JuliaOjedaAlonso <167695733+JuliaOjedaAlonso@users.noreply.github.com>
2024-12-05 15:54:23 +0100

    Update interface_v2.py

--- interface_v2.py
+++ interface_v2.py
@@ -16,7 +16,7 @@ def preprocess_image(image):
     image = image.resize((150, 150))
     return np.array(image)
 
-def send_image_to_api(image):
+def send_image_to_api(image_path):
     """
     Send the preprocessed image to the FastAPI endpoint and get predictions.
     """
@@ -31,8 +31,8 @@ def send_image_to_api(image):
 
     image_path= "out.png"
 
-    with open(image, 'rb') as file:
-        images = {'file': (image, file, 'image/png')}
+    with open(image_path, 'rb') as file:
+        images = {'file': (image_path, file, 'image/png')}
         response = requests.post(API_URL, files=images)
 
 
