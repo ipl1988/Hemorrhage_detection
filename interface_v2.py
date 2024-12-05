ea2bcc250bbe9cac0407b820144bce30531e6fcc
Julia Ojeda Alonso <167695733+JuliaOjedaAlonso@users.noreply.github.com>
2024-12-05 14:16:11 +0100

    interface new v2 version

--- interface_v2.py
+++ interface_v2.py
@@ -173,7 +173,7 @@ def main():
                 result = send_image_to_api(image)
 
                 if result is not None:
-                    if result == 0:
+                    if result == range(0.5,1):
                         st.success('Positive: high probability to find a hemorrhage in this image.')
                     else:
                         st.success('Negative: low probability to find a hemorrhage in this image.')
