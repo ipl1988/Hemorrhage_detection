4827b62261a3d3d446b7925570aa247e84207825
JuliaOjedaAlonso <167695733+JuliaOjedaAlonso@users.noreply.github.com>
2024-12-05 17:06:59 +0100

    Update interface_v2.py

--- interface_v2.py
+++ interface_v2.py
@@ -24,15 +24,15 @@ def send_image_to_api(image_path):
     image.save(image_bytes, format="PNG")  # Guardar como PNG
     image_bytes.seek(0)  # Ir al inicio del flujo
 
-    # Send request to API
-    files = {"file": (image_bytes, "image/png")}
-    response = requests.post(API_URL, files=files)
-    Open the file and send it as a request:
-        images = {'file': (image, file, 'image/png')}  
+    # # Send request to API
+    # files = {"file": (image_bytes, "image/png")}
+    # response = requests.post(API_URL, files=files)
+    # Open the file and send it as a request:
+    #     images = {'file': (image, file, 'image/png')}  
 
     #image_path = 'out.png'
-    with open(image_path, 'rb') as file:
-        images = {'file': (image_path, file, 'image/png')}
+    with open(image_bytes, 'rb') as file:
+        images = {'file': (image_bytes, file, 'image/png')}
         response = requests.post(API_URL, files=images)
 
 
