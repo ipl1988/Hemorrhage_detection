04f39bbcee085c9fc12754bb9905f48d69bafa2b
JuliaOjedaAlonso <167695733+JuliaOjedaAlonso@users.noreply.github.com>
2024-12-05 16:54:59 +0100

    Update interface_v2.py

--- interface_v2.py
+++ interface_v2.py
@@ -187,9 +187,9 @@ def main():
            # except Exception as e:
                # st.error(f"An error occurred while processing the image: {e}")
 
-        st.markdown("---")
-        st.markdown("### About")
-        st.text("This tool uses a CNN deep learning model to predict injury types from uploaded images.")
+    st.markdown("---")
+    st.markdown("### About")
+    st.text("This tool uses a CNN deep learning model to predict injury types from uploaded images.")
 
 if __name__ == "__main__":
     main()
